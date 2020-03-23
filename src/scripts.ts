aa61e4a05fe13ee7d3bb315825f22586809a0286
Jacqueline Hardy <jahardy@expedia.com>
2020-03-23 22:08:59 +1000

    feat: initial cut of reveal feature

---
+++ src/scripts.ts
@@ -0,0 +1,27 @@
+const reveal = Array.from(document.getElementsByClassName('reveal'))[0];
+const placeholders = Array.from(reveal.getElementsByClassName('reveal__placeholder'));
+const layers = Array.from(reveal.getElementsByClassName('reveal__layer'));
+
+window.addEventListener('scroll', handleScroll);
+
+function handleScroll() {
+  placeholders.forEach((placeholder, index) => {
+    const image = Array.from(layers[index].getElementsByClassName('reveal__image'))[0];
+    const rect = placeholder.getBoundingClientRect();
+    const top = rect.top - rect.height;
+    let offset = 0;
+
+    if (top + rect.height <= 0) {
+      offset = rect.height;
+    } else if (top <= 0) {
+      offset = top * -1;
+    }
+
+    setPosition(layers[index], offset);
+    setPosition(image, Math.abs(offset - rect.height));
+  });
+}
+
+function setPosition(element, position) {
+  element.style.transform = `translate3d(0, ${position}px, 0)`;
+}
