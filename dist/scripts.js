fde7e9352ec887a81c72587bb5dd14bb64450860
Jacqueline Hardy <jahardy@expedia.com>
2020-03-23 23:31:45 +1000

    refactor: clean up to implement class and automate placeholder logic

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,24 +1,47 @@
-const reveal = Array.from(document.getElementsByClassName('reveal'))[0];
-const placeholders = Array.from(reveal.getElementsByClassName('reveal__placeholder'));
-const layers = Array.from(reveal.getElementsByClassName('reveal__layer'));
-window.addEventListener('scroll', handleScroll);
-function handleScroll() {
-    placeholders.forEach((placeholder, index) => {
-        const image = Array.from(layers[index].getElementsByClassName('reveal__image'))[0];
-        const rect = placeholder.getBoundingClientRect();
-        const top = rect.top - rect.height;
-        let offset = 0;
-        if (top + rect.height <= 0) {
-            offset = rect.height;
+var CssClasses;
+(function (CssClasses) {
+    CssClasses["Layer"] = "reveal__layer";
+    CssClasses["Image"] = "reveal__layer__image";
+    CssClasses["Placeholder"] = "reveal__placeholder";
+})(CssClasses || (CssClasses = {}));
+class Reveal {
+    constructor(wrapper) {
+        this.placeholders = [];
+        this.wrapper = wrapper;
+        this.setupReveal();
+        window.addEventListener('scroll', () => window.requestAnimationFrame(this.handleScroll.bind(this)));
+    }
+    setupReveal() {
+        this.layers = Array.from(this.wrapper.getElementsByClassName(CssClasses.Layer));
+        let placeholder;
+        for (let i = 0; i < this.layers.length; i++) {
+            placeholder = document.createElement('div');
+            placeholder.classList.add(CssClasses.Placeholder);
+            this.wrapper.append(placeholder);
+            this.placeholders.push(placeholder);
         }
-        else if (top <= 0) {
-            offset = top * -1;
-        }
-        setPosition(layers[index], offset);
-        setPosition(image, Math.abs(offset - rect.height));
-    });
+    }
+    ;
+    handleScroll() {
+        this.placeholders.forEach((placeholder, index) => {
+            const layer = this.layers[index];
+            const image = layer.getElementsByClassName(CssClasses.Image)[0];
+            const rect = placeholder.getBoundingClientRect();
+            const top = rect.top - rect.height;
+            let offset = 0;
+            if (top + rect.height <= 0) {
+                offset = rect.height;
+            }
+            else if (top <= 0) {
+                offset = top * -1;
+            }
+            this.setPosition(layer, offset);
+            this.setPosition(image, Math.abs(offset - rect.height));
+        });
+    }
+    setPosition(element, position) {
+        element.style.transform = `translate3d(0, ${position}px, 0)`;
+    }
 }
-function setPosition(element, position) {
-    element.style.transform = `translate3d(0, ${position}px, 0)`;
-}
+new Reveal(document.getElementById('your-reveal-section'));
 //# sourceMappingURL=scripts.js.map
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,27 +1,54 @@
-const reveal = Array.from(document.getElementsByClassName('reveal'))[0];
-const placeholders = Array.from(reveal.getElementsByClassName('reveal__placeholder'));
-const layers = Array.from(reveal.getElementsByClassName('reveal__layer'));
+enum CssClasses {
+  Layer = 'reveal__layer',
+  Image = 'reveal__layer__image',
+  Placeholder = 'reveal__placeholder'
+}
 
-window.addEventListener('scroll', handleScroll);
+class Reveal {
+  wrapper: Element;
+  layers: Element[];
+  placeholders: Element[] = [];
 
-function handleScroll() {
-  placeholders.forEach((placeholder, index) => {
-    const image = Array.from(layers[index].getElementsByClassName('reveal__image'))[0];
-    const rect = placeholder.getBoundingClientRect();
-    const top = rect.top - rect.height;
-    let offset = 0;
+  constructor(wrapper: Element) {
+    this.wrapper = wrapper;
+    this.setupReveal();
+    window.addEventListener('scroll', () => window.requestAnimationFrame(this.handleScroll.bind(this)));
+  }
 
-    if (top + rect.height <= 0) {
-      offset = rect.height;
-    } else if (top <= 0) {
-      offset = top * -1;
+  setupReveal(): void {
+    this.layers = Array.from(this.wrapper.getElementsByClassName(CssClasses.Layer));
+
+    let placeholder: Element;
+    for (let i = 0; i < this.layers.length; i++) {
+      placeholder = document.createElement('div');
+      placeholder.classList.add(CssClasses.Placeholder);
+      this.wrapper.append(placeholder);
+      this.placeholders.push(placeholder);
     }
+  };
 
-    setPosition(layers[index], offset);
-    setPosition(image, Math.abs(offset - rect.height));
-  });
+  handleScroll(): void {
+    this.placeholders.forEach((placeholder: Element, index: number) => {
+      const layer: Element = this.layers[index];
+      const image: Element = layer.getElementsByClassName(CssClasses.Image)[0];
+      const rect: DOMRect = placeholder.getBoundingClientRect();
+      const top: number = rect.top - rect.height;
+      let offset: number = 0;
+
+      if (top + rect.height <= 0) {
+        offset = rect.height;
+      } else if (top <= 0) {
+        offset = top * -1;
+      }
+
+      this.setPosition(layer, offset);
+      this.setPosition(image, Math.abs(offset - rect.height));
+    });
+  }
+
+  setPosition(element: Element, position: number): void {
+    (element as HTMLElement).style.transform = `translate3d(0, ${position}px, 0)`;
+  }
 }
 
-function setPosition(element, position) {
-  element.style.transform = `translate3d(0, ${position}px, 0)`;
-}
+new Reveal(document.getElementById('your-reveal-section'));
